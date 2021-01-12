b4f69f36c0583b91a5456febf00fbffa686339ca
monukumar98 <monu012as@gmail.com>
2021-01-13 01:21:11 +0530

    Monu Kumar

---
+++ 1stjs/index.js
@@ -0,0 +1,35 @@
+"use strict"
+/*
+alert("hey monu");
+alert(3+1+8);
+// this is comment 
+/*
+jsaj
+sabbasb
+*/ 
+//alert("3+1+8"); 
+let message = "hey arpit";  // variable
+alert(message);
+message="priya";
+alert(message);
+let movie="dge";
+movie ="jvjv";
+// constant 
+const dataofborth = "10-09-2000";
+//dataofborth="11-09-2000";
+console.log(dataofborth);
+console.log("hey");
+console.log(movie);
+// number
+let ll = 10;
+console.log(10/2)
+let num = 1;
+console.log(num/0);
+console.log(-1/0);
+console.log("monu"/2);// NaN
+let isgreater = 9>7;
+console.log(isgreater);
+// ways to intreact with your browser
+let age = prompt("how old are you");
+let isboos= confirm("Are suure");
+alert(isboos);
