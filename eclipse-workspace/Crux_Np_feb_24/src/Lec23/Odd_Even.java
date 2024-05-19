2181d994fdac5ca2189d76b6a27845763681c70a
monukumar98 <monu012as@gmail.com>
2024-05-19 15:59:41 +0530

    Bit-Masking

--- eclipse-workspace/Crux_Np_feb_24/src/Lec23/Odd_Even.java
+++ eclipse-workspace/Crux_Np_feb_24/src/Lec23/Odd_Even.java
@@ -1,17 +1,14 @@
-package Lec1;
+package Lec23;
 
 public class Odd_Even {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 58;
-		if (n > 67) {
-			System.out.println("hello");
-		}
-		if (n % 2 == 0) {
-			System.out.println("even");
-		} else {
+		int n = 56;
+		if ((n & 1) != 0) {
 			System.out.println("odd");
+		} else {
+			System.out.println("even");
 		}
 
 	}
