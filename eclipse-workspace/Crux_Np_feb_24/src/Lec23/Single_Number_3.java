69ed05e622157693137cb9be7ef85e3f0896e103
monukumar98 <monu012as@gmail.com>
2024-05-19 16:04:47 +0530

    Bit-Masking

--- eclipse-workspace/Crux_Np_feb_24/src/Lec23/Single_Number_3.java
+++ eclipse-workspace/Crux_Np_feb_24/src/Lec23/Single_Number_3.java
@@ -4,12 +4,20 @@ public class Single_Number_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] arr= {1,2,1,3,2,5};
-		int ans=0;
+		int[] arr = { 1, 2, 1, 3, 2, 5 };
+		int ans = 0;
 		for (int i = 0; i < arr.length; i++) {
-			ans= ans^arr[i];
+			ans = ans ^ arr[i];
 		}
-		System.out.println(ans);
+		int mask = (ans * (~(ans - 1)));
+		int a = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if ((arr[i] & mask) == 0) {
+				a ^= arr[i];
+			}
+		}
+		int b = ans ^ a;
+		int[] ar = { a, b };
 
 	}
 
